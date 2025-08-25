e476eb91c2e1d20f212394aa47ebbe5a522115e0
LukeTandjung <lukelucus123@gmail.com>
2025-08-25 21:52:07 +0100

    Added fastfetch, removed spotify-player

--- home.nix
+++ home.nix
@@ -133,12 +133,99 @@
   programs.fastfetch = {
     enable = true;
     settings = {
+      "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
         source = "./hokusai.jpg";
+        padding = {
+          top = 2;
+        };
       };
-
+      modules = [
+        "title"
+        "separator"
+        "os"
+        "host"
+        "bios"
+        "bootmgr"
+        "board"
+        "chassis"
+        "kernel"
+        "initsystem"
+        "uptime"
+        "loadavg"
+        "processes"
+        "packages"
+        "shell"
+        "editor"
+        "display"
+        "brightness"
+        "monitor"
+        "lm"
+        "de"
+        "wm"
+        "wmtheme"
+        "theme"
+        "icons"
+        "font"
+        "cursor"
+        "wallpaper"
+        "terminal"
+        "terminalfont"
+        "terminalsize"
+        "terminaltheme"
+        { type = "cpu"; showPeCoreCount = true; temp = true; }
+        "cpucache"
+        "cpuusage"
+        { type = "gpu"; driverSpecific = true; temp = true; }
+        "memory"
+        "physicalmemory"
+        { type = "swap"; separate = true; }
+        "disk"
+        "btrfs"
+        "zpool"
+        { type = "battery"; temp = true; }
+        "poweradapter"
+        "player"
+        "media"
+        { type = "publicip"; timeout = 1000; }
+        {
+          type = "localip";
+          showIpv6 = true;
+          showMac = true;
+          showSpeed = true;
+          showMtu = true;
+          showLoop = true;
+          showFlags = true;
+          showAllIps = true;
+        }
+        "dns"
+        "wifi"
+        "datetime"
+        "locale"
+        "vulkan"
+        "opengl"
+        "opencl"
+        "users"
+        "bluetooth"
+        "bluetoothradio"
+        "sound"
+        "camera"
+        "gamepad"
+        "mouse"
+        "keyboard"
+        { type = "weather"; timeout = 1000; }
+        "netio"
+        "diskio"
+        { type = "physicaldisk"; temp = true; }
+        "tpm"
+        "version"
+        "break"
+        "colors"
+      ];
     };
-  };
+  }
+
+  
   # Zellij Configuration
   programs.zellij = {
     enable = true;
@@ -146,9 +233,6 @@
     attachExistingSession = true;
     exitShellOnExit = true;
   };
-
-  # Spotify-player Configuration
-  programs.spotify-player.enable = true;
 
   # LazyDocker Configuration
   programs.lazydocker.enable = true;
